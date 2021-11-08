6e5cdae61ac5240937ef823e72a705ffedc93de8
Ilyasov23 <milyasav@mail.ru>
2021-11-08 21:44:50 +0500

    Добавил навигацю на каждую страницу сайта

--- php/index.php
+++ php/index.php
@@ -1,19 +1,20 @@
 <ul>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-1-1.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-1-2.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-1-3.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-1-4.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-1.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-2.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-3.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-4.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-5.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-6.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-7.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-8.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-9.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-10.php</li>
-  <li>http://ilyasovmax/github/IlyasovMR_PI321_LabsForIP/php/lab-2-11.php</li>
+  Вставь нужную ссылку в поисковик для перехода по старницам сайта
+  <li>http://f0597837.xsph.ru/lab-1-1.php</li>
+  <li>http://f0597837.xsph.ru/lab-1-2.php</li>
+  <li>http://f0597837.xsph.ru/lab-1-3.php</li>
+  <li>http://f0597837.xsph.ru/lab-1-4.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-1.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-2.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-3.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-4.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-5.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-6.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-7.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-8.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-9.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-10.php</li>
+  <li>http://f0597837.xsph.ru/lab-2-11.php</li>
 
 
 <p> Дата и время:
