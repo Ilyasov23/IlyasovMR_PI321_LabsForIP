52a58062e7d5346bd4dbdd2119ca5572200af624
Ilyasov23 <milyasav@mail.ru>
2021-11-08 21:09:00 +0500

    Создал таблицу 10×10 с числами от 1 до 100. Четные - красные.

---
+++ php/lab-2-10.php
@@ -0,0 +1,21 @@
+<HTML>
+  <TITLE> Таблица умножения </TITLE>
+  <BODY>
+    <TABLE border=1>
+      <?php
+//echo '<table border=1>';
+       for ($i = 0; $i < 10; $i++)
+       {
+         echo '<tr>';
+         for ($k = 1; $k < 11; $k++)
+         {
+           echo '<td style="color:'.($k%2?'black':'red').'">'.($i*10+$k).'</td>';
+         }
+         echo '</tr>';
+       }
+       echo '</table>';
+       ?>
+  </TABLE>
+</BODY>
+</HTML>
+     
