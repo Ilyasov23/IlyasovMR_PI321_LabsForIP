8cf6297ae8d7948d85b7e5b307066f6eff81d89f
Ilyasov23 <milyasav@mail.ru>
2021-11-08 20:33:41 +0500

    Написал скрипт с числом e и изменением типов  переменных

---
+++ php/lab-1-3.php
@@ -0,0 +1,23 @@
+<p> Мат. операция:
+<p>
+<?php
+$NUM_E=(M_E);
+
+echo ('Namber e' . ' = ' . $NUM_E . '<br>');
+
+$num_e1=($NUM_E);
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+$num_e1 = (string) $num_e1;
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+$num_e1 = (int) $num_e1;
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+$num_e1 = (float) $num_e1;
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+$num_e1 = (string) $num_e1;
+echo ('$num_e1' . ' = ' . $num_e1 . ' - ');
+echo gettype($num_e1) . '<br>';
+?>
