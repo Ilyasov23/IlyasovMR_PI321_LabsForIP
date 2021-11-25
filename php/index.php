d8487a04920f38692f6d3a06f31a97f83cd8d688
Ilyasov23 <milyasav@mail.ru>
2021-11-25 17:22:34 +0500

    1. Создал скрипт с массивами по 6 зададнями. 2. Обновил всю навигацию на сайте.

--- php/index.php
+++ php/index.php
@@ -23,3 +23,51 @@
  $d=date("d.m.Y H:i");
  echo($d);
 ?>
+<div id="bubble">
+    <p><a href="http://f0597837.xsph.ru/lab-1-1.php"> 1 </a></p>
+  </div>
+  <div id="bubble">
+      <p><a href="http://f0597837.xsph.ru/lab-1-2.php"> 2 </a></p>
+    </div>
+    <div id="bubble">
+        <p><a href="http://f0597837.xsph.ru/lab-1-3.php"> 3 </a></p>
+      </div>
+      <div id="bubble">
+          <p><a href="http://f0597837.xsph.ru/lab-1-4.php"> 4 </a></p>
+        </div>
+        <div id="bubble">
+            <p><a href="http://f0597837.xsph.ru/lab-2-1.php"> 5 </a></p>
+          </div>
+          <div id="bubble">
+              <p><a href="http://f0597837.xsph.ru/lab-2-2.php"> 6</a></p>
+            </div>
+            <div id="bubble">
+                <p><a href="http://f0597837.xsph.ru/lab-2-3.php"> 7 </a></p>
+              </div>
+              <div id="bubble">
+                  <p><a href="http://f0597837.xsph.ru/lab-2-4.php"> 8 </a></p>
+                </div>
+                <div id="bubble">
+                    <p><a href="http://f0597837.xsph.ru/lab-2-5.php"> 9 </a></p>
+                  </div>
+                  <div id="bubble">
+                      <p><a href="http://f0597837.xsph.ru/lab-2-6.php"> 10 </a></p>
+                    </div>
+                    <div id="bubble">
+                        <p><a href="http://f0597837.xsph.ru/lab-2-7.php"> 11 </a></p>
+                      </div>
+                      <div id="bubble">
+                          <p><a href="http://f0597837.xsph.ru/lab-2-8.php"> 12 </a></p>
+                        </div>
+                        <div id="bubble">
+                            <p><a href="http://f0597837.xsph.ru/lab-2-9.php"> 13 </a></p>
+                          </div>
+                          <div id="bubble">
+                              <p><a href="http://f0597837.xsph.ru/lab-2-10.php"> 14 </a></p>
+                            </div>
+                            <div id="bubble">
+                                <p><a href="http://f0597837.xsph.ru/lab-2-11.php"> 15 </a></p>
+                              </div>
+                              <div id="bubble">
+                                  <p><a href="http://f0597837.xsph.ru/lab2-1.php"> 16 </a></p>
+                                </div>
