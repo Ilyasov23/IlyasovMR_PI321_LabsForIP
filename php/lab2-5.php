b725157c488cffbead1331b5500cf4e9750922a2
Ilyasov23 <milyasav@mail.ru>
2021-12-05 16:16:37 +0500

    Добавил 11 вариант

--- php/lab2-5.php
+++ php/lab2-5.php
@@ -3,36 +3,63 @@
   </div>
   <h1 align="center"> Лабораторная работа №2 </h1>
   <h1 align="center"> Самотоятельная работа №2. </h1>
-  <h2 align="center"> Вариант 14 </h2>
+
+  <head>
+  <title>Ильясов М.Р. ПИ-321</title>
+ <body bgcolor="#A9A9A9">
+</head>
+
+<h2>Вариант 11</h2>
+<h3> Найти значение переменной z, заданной суммой функций. Для нахождения значения </h3>
+<h3>функции f(u,t) написать пользовательскую функцию. Числа a и b – случайные.</h3>
   <?php
 
   function f($u, $t) {
-      if ( $u>2)
-          return pow($u^2+$t,1/3)*pow(sin($t),2);
-      elseif ( $u <= 2)
-          return log(abs($u^2/($u+$t)));
+    if ( $u>2)
+        return pow($u^2+$t,1/3)*pow(sin($t),2);
+    elseif ( $u <= 2)
+        return log(abs($u^2/($u+$t)));
 
-  }
+}
+
+
+  echo "<img src='forlab2.png'>";
+$A=rand(-10,10);
+$B=rand(-10,10);
+
+echo "<br>","A = $A;","<br>","B = $B","<br>";
+
+$result=sin(f($B,$A))+sqrt(abs(f($A,$B)));
+echo "Результат = ","$result";
+
+  ?>
+
+
+  <h2>Вариант 14</h2>
+  <h3> Найти значение переменной z, заданной суммой функций. Для нахождения значения </h3>
+  <h3>функции f(u,t) написать пользовательскую функцию. Числа a и b – случайные.</h3>
+  <?php
 
   function f2($u, $t) {
-     if ( $u < -2 )
-         return 2*$u;
-     elseif ( $u >= -2 && $u <= 2 )
-         return $t;
-     elseif ( $u >= 0 && $t < 0 )
-         return sin(pow($u,3))+log(abs($t));
-       elseif ( $u >2)
-         return pow(pow(cos($u),2)+pow(sin(pow($t,3)),4),1/4);}
+    if ( $u < -2 )
+        return 2*$u;
+    elseif ( $u >= -2 && $u <= 2 )
+        return $t;
+    elseif ( $u >= 0 && $t < 0 )
+        return sin(pow($u,3))+log(abs($t));
+      elseif ( $u >2)
+        return pow(pow(cos($u),2)+pow(sin(pow($t,3)),4),1/4);}
 
 
-   echo "<img src='forlab2.png'>";
- $A=rand(-10,10);
- $B=rand(-10,10);
 
- echo "<br>","a = $A;","<br>","b = $B","<br>";
+echo "<img src='forlab2.1.png'>";
+$A=rand(-10,10);
+$B=rand(-10,10);
 
- $result=log(abs(f2($A,$B)))+f(pow($A,2),$A+$B);
- echo "Результат = ","$result";
+echo "<br>","A = $A;","<br>","B = $B","<br>";
+
+$result=log(abs(f2($A,$B)))+f(pow($A,2),$A+$B);
+echo "Результат = ","$result";
   ?>
   <div id="bubble">
       <p><a href="http://f0597837.xsph.ru/lab2-6.php"> 	&raquo; Самостоятельная работа №3 </a></p>
